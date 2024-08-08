968aea00927ab341a434f3b7799afcfd66367cab
talentlessDeveloper <kareemope52@gmail.com>
2024-08-08 21:35:29 +0100

    feat: signup functionality

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -1,20 +1,31 @@
-"use server";
-
 import { AxiosError } from "axios";
+import { getCookie, setCookie } from "cookies-next";
 import { axiosInstance } from "~/lib/axiosInstance";
 
 type RegisterUser = {
   email: string;
   password: string;
   confirm_password: string;
+  full_name: string;
+  address: string;
+  phone_number: string;
 };
 export const registerUser = async ({
   email,
   password,
   confirm_password,
+  address,
+  phone_number,
+  full_name,
 }: RegisterUser) => {
   if (!email) {
     return { status: "error", message: "Email is missing" };
+  }
+  if (!address) {
+    return { status: "error", message: "Address is missing" };
+  }
+  if (!full_name) {
+    return { status: "error", message: "Full name is missing" };
   }
   if (password !== confirm_password) {
     return {
@@ -49,5 +60,26 @@ export const registerUser = async ({
       };
     }
     return { status: "error", message: "Internal Server Error" };
+  }
+};
+
+export const authRefresh = async () => {
+  const refreshToken = getCookie("intellibin_refresh_token");
+  setCookie("intellibin_token", "");
+  try {
+    ("inside authRefresh");
+    const response = await axiosInstance.post("api/token/refresh", {
+      refreshToken: refreshToken,
+    });
+
+    const { access } = response.data;
+    setCookie("intellibin_token", access);
+
+    return access;
+  } catch (error) {
+    console.log(
+      "error setting cookie",
+      error instanceof Error ? error.message : "something wrong",
+    );
   }
 };
